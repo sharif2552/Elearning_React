e70bc69f402395097c3de8b30c8b2b8554ddb650
sharif2552 <86832372+sharif2552@users.noreply.github.com>
2024-02-22 02:36:35 +0600

    update

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 
 import './App.css';
+import Login from './pages/Login';
 
 function App() {
   return (
     <div className="App">
-      <h1>React App</h1>
+      <Login />
+
     </div>
   );
 }
