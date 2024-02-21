22c7c04aaa0d9612b1ea3133354e058e6432473e
sharif2552 <86832372+sharif2552@users.noreply.github.com>
2024-02-22 03:47:11 +0600

    Update route path in App.js

--- src/App.js
+++ src/App.js
@@ -11,7 +11,7 @@ import { createBrowserRouter, RouterProvider } from "react-router-dom";
 
 const router = createBrowserRouter([
   {
-    path: "/",
+    path: "/login",
     element: <Login />,
   },
   {
