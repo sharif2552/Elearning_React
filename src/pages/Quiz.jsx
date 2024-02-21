d2726aa2d326cf2876401e88bbcc1024243d7c3a
sharif2552 <86832372+sharif2552@users.noreply.github.com>
2024-02-22 04:30:42 +0600

    Add TestComponent and update dependencies

--- src/pages/Quiz.jsx
+++ src/pages/Quiz.jsx
@@ -1,9 +1,16 @@
 import React from 'react';
+import "@fontsource/roboto/300.css";
+import "@fontsource/roboto/400.css";
+import "@fontsource/roboto/500.css";
+import "@fontsource/roboto/700.css";
+import TestComponent from "../components/TestComponent";
+
 
 const Quiz = () => {
     return (
         <div>
             quiz page
+            <TestComponent />
         </div>
     );
 };
