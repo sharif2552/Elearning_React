4f77f4966343951133541143421254357361e84b
sharif2552 <86832372+sharif2552@users.noreply.github.com>
2024-06-10 21:59:31 +0600

    chore: Add registration functionality to Navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,12 +1,19 @@
 import React, { useState } from "react";
 import { HiMenu, HiX } from "react-icons/hi";
+import { Link, useNavigate } from "react-router-dom";
 import logo from "../assets/img/navbar/logo.png";
+import Register from "../pages/Signup";
 
 const Navbar = () => {
   const [showMenu, setShowMenu] = useState(false);
+  const navigate = useNavigate();
 
   const toggleMenu = () => {
     setShowMenu(!showMenu);
+  };
+
+  const handleRegisterClick = () => {
+    navigate("/signup"); // Adjust this path if needed
   };
 
   return (
@@ -20,7 +27,7 @@ const Navbar = () => {
         className="fixed top-0 left-0 w-full h-full z-50 flex justify-center items-center"
         style={{ display: showMenu ? "flex" : "none" }}
       >
-        <div className=" bg-transparent max-w-full backdrop-filter backdrop-blur-md flex align-middle justify-center w-full h-full text-center  rounded-md p-4 shadow-lg">
+        <div className="bg-transparent max-w-full backdrop-filter backdrop-blur-md flex align-middle justify-center w-full h-full text-center rounded-md p-4 shadow-lg">
           <div className="flex flex-col items-center justify-center space-y-4 mt-8">
             <a
               href="#"
@@ -50,7 +57,7 @@ const Navbar = () => {
         </div>
       </div>
       <div className="fixed top-0 left-0 w-full z-50 bg-transparent backdrop-filter backdrop-blur-md">
-        <nav className="container mx-auto flex justify-between p-2 ">
+        <nav className="container mx-auto flex justify-between p-2">
           <div className="flex items-center space-x-5">
             <img src={logo} alt="logo" className="w-20 h-20" />
             <h1 className="text-black text-4xl font-bold font-raleway">
@@ -96,10 +103,16 @@ const Navbar = () => {
             </a>
           </div>
           <div className="hidden lg:flex items-center space-x-5">
-            <button className="text-secondary-replyGreen-900 font-raleway bg-white px-7 py-3 rounded-md">
+            <Link
+              to="/login"
+              className="text-secondary-replyGreen-900 font-raleway bg-white px-7 py-3 rounded-md"
+            >
               Log In
-            </button>
-            <button className=" bg-secondary-replyGreen-900 font-raleway text-white px-7 py-3 rounded-md">
+            </Link>
+            <button
+              className="bg-secondary-replyGreen-900 font-raleway text-white px-7 py-3 rounded-md"
+              onClick={handleRegisterClick}
+            >
               Register
             </button>
           </div>
