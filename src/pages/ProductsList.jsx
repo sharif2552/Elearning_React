89c57a27821f15f9f4c15f70ca7f5ec6d63005be
sharif2552 <86832372+sharif2552@users.noreply.github.com>
2025-05-17 21:34:14 +0600

    updated formatting

--- src/pages/ProductsList.jsx
+++ src/pages/ProductsList.jsx
@@ -2,41 +2,33 @@ import React, { useState, useEffect } from "react";
 import axios from "axios";
 
 const ProductsList = () => {
-  // State variables to manage image data
+  const [allImage, setAllImage] = useState(null);
 
-  const [allImage, setAllImage] = useState(null); // State for all images
-
-
-  // Function to fetch image data when component mounts
   useEffect(() => {
     getImage();
   }, []);
 
-  // Function to fetch all images from server
   const getImage = async () => {
     const result = await axios.get("http://localhost:5000/api/products");
     console.log(result);
-    setAllImage(result.data.data); // Update state with received image data
+    setAllImage(result.data.data);
   };
 
   return (
     <div>
-      {/* Display uploaded images */}
       {allImage == null
-        ? "" // If no images, show empty string
+        ? ""
         : allImage.map((data, index) => {
-            // Map through all images and display them
             return (
               <div key={index}>
                 <img
-                  //${data.image}
-                  src={require(`../assets/images/${data.image}`)} // Display image using require
+                  src={require(`../assets/images/${data.image}`)}
                   height={100}
                   width={100}
-                  alt={data.title} // Set alt attribute to the title of the image
+                  alt={data.title}
                 />
-                <p>Title: {data.title}</p> {/* Display title */}
-                <p>Description: {data.description}</p> {/* Display description */}
+                <p>Title: {data.title}</p>
+                <p>Description: {data.description}</p>
                 <p>Price: {data.price}</p>
               </div>
             );
